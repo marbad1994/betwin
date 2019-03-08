ad0d5559b7ea093143823defe4a370d7d64d5374
marbad1994 <marcusbader@hotmail.com>
2019-03-08 18:22:34 +0100

    added safe bet

--- app.py
+++ app.py
@@ -1,19 +1,20 @@
 from flask import Flask, render_template, request
 from betting_2 import main as bet
+from betsc import main as safe
 
 app = Flask(__name__)
 
 @app.route("/", methods=["GET", "POST"])
 def index():
-    if request.method == "POST":
+    if request.method == "POST" and request.form.get('one_bet') is not None:
         one = request.form.get('one_bet')
         two = request.form.get('two_bet')
         cross = request.form.get('cross_bet')
-        if "," in one:
+        if one is not None and "," in one:
             one = one.replace(",", ".")
-        if "," in two:
+        if two is not None and "," in two:
             two = two.replace(",", ".")
-        if "," in cross: 
+        if cross is not None and "," in cross: 
             cross = cross.replace(",", ".")
         chars = "abcdefghijklmnopqrstuvxyzABCDEFGHIJKLMNOPQRSTUVXYZ"    
         for i in chars:
@@ -22,6 +23,24 @@ def index():
         bonus = request.form.get('bonus')
         bettson = bet(float(one),float(cross),float(two), float(bonus))
         return render_template("index.html", bettson=bettson)
+    elif request.method == "POST" and request.form.get('one_bet_t') is not None:
+        one = request.form.get('one_bet_t')
+        two = request.form.get('two_bet_t')
+        cross = request.form.get('cross_bet_t')
+        if one is not None and "," in one:
+            one = one.replace(",", ".")
+        if two is not None and "," in two:
+            two = two.replace(",", ".")
+        if cross is not None and "," in cross: 
+            cross = cross.replace(",", ".")
+        chars = "abcdefghijklmnopqrstuvxyzABCDEFGHIJKLMNOPQRSTUVXYZ"    
+        for i in chars:
+            if i in one or i in two or i in cross:
+                return render_template("index.html", bettson=["Fel","", "", ""])
+        bonus = request.form.get('bonus_t')
+        bettson = safe(float(one),float(cross),float(two), float(bonus))
+        return render_template("index.html", bettson=bettson)
+    
     else:
         return render_template("index.html", bettson="")
 if __name__ == '__main__':
